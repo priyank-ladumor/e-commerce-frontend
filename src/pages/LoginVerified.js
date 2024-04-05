b3c576211820829cbd4835266058daa7c15f9c33
priyank ladumor <137873635+priyank-ladumor@users.noreply.github.com>
2024-04-05 16:00:58 +0530

    login verified page skeleton setup

--- src/pages/LoginVerified.js
+++ src/pages/LoginVerified.js
@@ -3,13 +3,14 @@ import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useParams } from 'react-router-dom'
 import { userAccountVerified } from '../store/action/authAction'
 import { getLogoAction } from '../store/action/bannerLogoAction'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 
 const LoginVerified = () => {
     const { token } = useParams()
     const dispatch = useDispatch()
 
     const [getLogo, setgetLogo] = useState("")
-    const { getLogoDATA, addLogoMSG } = useSelector((state) => state.bannerLogo)
+    const { getLogoDATA, addLogoMSG, getLogoPENDING } = useSelector((state) => state.bannerLogo)
   
     useEffect(() => {
       dispatch(getLogoAction())
@@ -31,11 +32,18 @@ const LoginVerified = () => {
             <div className=" min-h-full h-screen  px-6 py-12  grid place-content-center md:me-[200px]">
                 <div className="border-2 border-gray-300 bg-white py-12 px-8 w-[100%] md:w-[150%] ">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                        <img
-                            className="mx-auto h-16 rounded-lg w-auto"
-                            src={getLogo?.logo}
-                            alt="Your Company"
-                        />
+                        {getLogoPENDING
+                            ?
+                            <SkeletonTheme baseColor="whitesmoke" highlightColor="#fff">
+                                <p className='p-2 flex justify-center' >
+                                    <Skeleton count={1} className='rounded-lg' width={70} height={70} />
+                                </p>
+                            </SkeletonTheme>
+                            : <img
+                                className="mx-auto h-16 rounded-lg w-auto"
+                                src={getLogo?.logo}
+                                alt="Your Company"
+                            />}
                         <h2 className="mt-2 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
                             Shoppy.io
                         </h2>
